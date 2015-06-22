a0ef40c17f95c32029916f5ceadb2730725d7fba
senayzgn <1131722@stud.hs-mannheim.de>
2015-06-22 17:02:58 +0200

    Meilenstein6

--- Meilenstein6/buchanlegen.php
+++ Meilenstein6/buchanlegen.php
@@ -1,5 +1,6 @@
 <?php
-// Die mittels GET geschickten Daten des Anwenders werden in Variablen gespeichert.
+
+// Speichern der Daten in die Variablen
 $titel = $_GET["titel"];
 $autor = $_GET["autor"];
 $kapitel = $_GET["kapitel"];
@@ -11,35 +12,65 @@ $genre = $_GET["genre"];
 $vorname = $_GET["vorname"];
 $nachname = $_GET["name"];
 $favorit;
-// Hier nun Validierung der Daten des Anwenders!
+
 $aktuellesJahr = date("Y");
-if($jahr < -1 || $jahr > $aktuellesJahr){
+if($jahr < 0 || $jahr > $aktuellesJahr){
     $jahr = null;
-    echo $jahr;
 }
-// Ende der Validierung
+if(!preg_match("/^[A-Z][a-z]+$/",$vorname)){
+    $vorname = null;
+}
+if(!preg_match("^[A-Z][a-z]+$^",$nachname)){
+    $nachname = null;
+}
+if(!preg_match("/^[0-9]{1,13}$/",$isbn)){
+    $isbn = null;
+}
+if(!preg_match("/^[0-9]{1,3}$/",$auflage)){
+    $auflage = null;
+}
+
+if(!preg_match("/^[A-Z][a-z]+[\s]*[A-Z][a-z]*$/",$autor)){
+    $autor = "null";
+}
+
 if(isset($_GET["filmfavorit"])){
     $favorit = 1;
 }else{
     $favorit = 0;
 }
-// Herstellung der Verbindung mit der Datenbank, die auf localhost, mit dem Nutzer root und ohne Passwort laeuft.
+
+// Herstellung der DB Verbindung
 $datenbankVerbindung = mysql_connect("localhost", "root", "");
 mysql_set_charset('UTF-8', $datenbankVerbindung);
-// Die Datenbank "mybooks" wird gewaehlt.
+// Wahl der mybooks Datenbank
 mysql_select_db('mybooks') or die ("Fehler beim Zugriff auf die Datenbank!");
 mysql_query("SET NAMES 'utf8'");
-// String zur Eintragung in die Datenbank als MySQL-Befehl wird vorbereitet.
-$insertBuch = "INSERT INTO buch (titel, autor, isbn, kapitel, jahr, auflage, art, genre) VALUES ('$titel', '$autor','$isbn', '$kapitel', '$jahr', '$auflage', '$art', '$genre');";
-// Das Buch wird eingetragen.
+
+//String zur Eintragung in die DB
+$insertBuch = "INSERT INTO book (titel, autor, isbn, kapitel, jahr, auflage, art, genre) VALUES ('$titel', '$autor','$isbn', '$kapitel', '$jahr', '$auflage', '$art', '$genre');";
+// eintragen in die DB
 $eintragung = mysql_query($insertBuch);
-$selectBenutzer = "SELECT benutzerID FROM benutzer WHERE vorname = '".$vorname."' AND nachname = '". $nachname."';";
+
+$selectBenutzer = "SELECT benutzerID FROM user WHERE vorname = '".$vorname."' AND nachname = '". $nachname."';";
 $abfrageBenutzer = mysql_query($selectBenutzer);
+
 if(mysql_num_rows($abfrageBenutzer) == 0){
-    $insertBenutzer = "INSERT INTO benutzer (vorname, nachname) VALUES ('$vorname','$nachname');";
+    $insertBenutzer = "INSERT INTO user (vorname, nachname) VALUES ('$vorname','$nachname');";
     $eintragungBenutzer = mysql_query($insertBenutzer);
+    $abfrageBenutzer = mysql_query($selectBenutzer);
+    $user = mysql_fetch_object($abfrageBenutzer);
+    $benutzerID = $user->benutzerID;
 }else{
-    $benutzer = mysql_fetch_object($abfrageBenutzer);
-    $benutzerID = $benutzer->benutzerID;
+    $user = mysql_fetch_object($abfrageBenutzer);
+    $benutzerID = $user->benutzerID;
 }
-?>
+
+$abfrageBuchID = "SELECT buchID from buch order by buchID desc limit 1;";
+$ergebnisBuchID = mysql_query($abfrageBuchID);
+$book = mysql_fetch_object($ergebnisBuchID);
+$buchID = $book->buchID;
+
+$insertBenutzerBuch = "INSERT INTO userbooks (benutzerID, buchID, favorit) VALUES ('$benutzerID','$buchID',$favorit)";
+
+
--- Meilenstein6/getBooks.php
+++ Meilenstein6/getBooks.php
@@ -2,12 +2,58 @@
 
 header('Content-Type: application/json');
 
-$_datei = $_GET["datei"];
-$_dateiname = $_datei.'books.json';
-$_buchArt = file_get_contents($_dateiname);
-
-echo $_buchArt;
 
 
+// Herstellung der Verbindung mit der Datenbank, die auf localhost, mit dem Nutzer root und ohne Passwort laeuft.
+$datenbankVerbindung = mysql_connect("localhost", "root", "");
+mysql_set_charset('UTF-8', $datenbankVerbindung);
+// Die Datenbank "mybooks" wird gewaehlt.
+mysql_select_db('mybooks') or die ("Fehler beim Zugriff auf die Datenbank!");
+mysql_query("SET NAMES 'utf8'");
 
+// Die beiden Strings mit dem MySQL-Befehl zur Abfrage der Datensaetze des jeweiligen Genres aus der Datenbank
+$abfrageHorror = "SELECT * FROM buch where genre = 'Horror';";
+$abfrageRoman = "SELECT * FROM buch where genre = 'Romanze';";
+
+erstelleJsonDatei("horror_books.json", $abfrageHorror, "horrordata");
+erstelleJsonDatei("roman_books.json", $abfrageRoman, "romandata");
+
+function erstelleJsonDatei($dateiname, $sqlBefehl, $genreName)
+{
+
+// JSON-Datei öffnen,
+    $jsonDatei = fopen($dateiname, "w");
+    fwrite($jsonDatei, "{\n\"".$genreName."\": [\n");
+    $abfrageErgebnisRoman = mysql_query($sqlBefehl);
+    $anzahlDatensaetze = mysql_num_rows($abfrageErgebnisRoman);
+    $zaehler = 0;
+    while ($datensatz = mysql_fetch_object($abfrageErgebnisRoman)) {
+        $zaehler++;
+        fwrite($jsonDatei, "{\n");
+        $autor = "\"autor\": \"" . $datensatz->autor . "\",\n";
+        fwrite($jsonDatei, $autor);
+        $titel = "\"titel\": \"" . $datensatz->titel . "\",\n";
+        fwrite($jsonDatei, $titel);
+        $kapitel = "\"kapitel\": " . $datensatz->kapitel . ",\n";
+        fwrite($jsonDatei, $kapitel);
+        $buchart = "\"buchart\": \"" . $datensatz->art . "\",\n";
+        fwrite($jsonDatei, $buchart);
+        $isbn = "\"ISBN\": " . $datensatz->isbn . ",\n";
+        fwrite($jsonDatei, $isbn);
+        $jahr = "\"erscheinungsjahr\": " . $datensatz->jahr . ",\n";
+        fwrite($jsonDatei, $jahr);
+        $auflage = "\"auflage\": " . $datensatz->auflage . "\n";
+        fwrite($jsonDatei, $auflage);
+        fwrite($jsonDatei, "}");
+        if ($zaehler != $anzahlDatensaetze) {
+            fwrite($jsonDatei, ",");
+        }
+    }
+    fwrite($jsonDatei, "\n]\n}");
+}
+$_file = $_GET["file"];
+$_filename = $_file.'books.json';
+$_genre = file_get_contents($_filename);
+
+echo $_genre;
 ?>
