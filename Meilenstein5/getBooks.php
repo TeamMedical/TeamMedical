d0b4d08a7a29165ad85f22ce1485ca1e6e53bd5c
senayzgn <1131722@stud.hs-mannheim.de>
2015-06-22 14:06:54 +0200

    Meilenstein5beendet

---
+++ Meilenstein5/getBooks.php
@@ -0,0 +1,13 @@
+<?php
+
+header('Content-Type: application/json');
+
+$_datei = $_GET["datei"];
+$_dateiname = $_datei.'books.json';
+$_buchArt = file_get_contents($_dateiname);
+
+echo $_buchArt;
+
+
+
+?>
